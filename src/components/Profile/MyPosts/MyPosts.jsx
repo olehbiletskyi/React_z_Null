b60a0403378d603d091cee59bf803e216dbb4b62
OlegBiletsky <ol.biletsky@gmail.com>
2020-06-01 16:23:35 +0300

    replace arrays

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -2,14 +2,11 @@ import React from 'react';
 import s from "./MyPosts.module.css";
 import Post from "./Post/Post";
 
-const MyPosts = () => {
+const MyPosts = (props) => {
 
-    let posts = [
-        {id: 1, message: 'Hey its my first post using PROPS', likeCounter: 101},
-        {id: 2, message: 'This is second post', likeCounter: 68} ,
-    ]
-
-    let postsElements = posts.map(p => <Post message={p.message} likeCounter={p.likeCounter} /> );
+    let postsElements = 
+    props.posta.map(p => <Post message={p.message} likeCounter={p.likeCounter} /> );
+    
     return(
         <div className={s.postsBlock}>
             
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -4,12 +4,17 @@ import MyPosts from './MyPosts/MyPosts';
 import ProfileInfo from './ProfileInfo/ProfileInfo';
 
 const Profile = () => {
+    let posts6 = [
+        {id: 1, message: 'Hey its my first post using PROPS', likeCounter: 101},
+        {id: 2, message: 'This is second post', likeCounter: 68} ,
+    ]
+
     return(
         <div >
 
-            <ProfileInfo/>
+            <ProfileInfo />
 
-            <MyPosts />
+            <MyPosts posts={posts6}  posta={000} posta={000}/>
 
         </div>
     )
