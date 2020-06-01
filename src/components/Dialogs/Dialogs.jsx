83fa32c69d1e6d49ab2d27883a8a241abeeef489
OlegBiletsky <ol.biletsky@gmail.com>
2020-06-01 09:26:00 +0300

    Виніс дані нв окремі Файли

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,22 +1,7 @@
 import React from 'react';
 import s from "./Dialogs.module.css";
-import { NavLink } from 'react-router-dom';
-
-
-const DialogItem = (props) => {
-    return (
-        <div className={s.dialog + ' ' + s.active}>
-            <NavLink to={"/dialogs/" + props.id}>{props.name}</NavLink>
-        </div>
-    )
-}
-
-const Message = (props) => {
-    return (
-        <div className={s.dialog}>{props.message}</div>
-    )
-}
-
+import DialogItem from './DialogItem/DialogItem';
+import Message from './Message/Message';
 
 
 const Dialogs = (props) => {
@@ -41,7 +26,6 @@ const Dialogs = (props) => {
     let dialogsElements = dialogs.map( d => <DialogItem name={d.name} id={d.id} /> );
 
     let messagesElements = messages.map( m => <Message message={m.message} /> );
-
 
     return (
         <div className={s.dialogs}>
