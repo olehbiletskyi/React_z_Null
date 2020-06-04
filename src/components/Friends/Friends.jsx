ae4335d66de89ddff7edcb1e75fdc6ba24a9402c
OlegBiletsky <ol.biletsky@gmail.com>
2020-06-04 14:09:06 +0300

    Friends.jsx and Frienditem.jsx

--- src/components/Friends/Friends.jsx
+++ src/components/Friends/Friends.jsx
@@ -2,45 +2,17 @@ import React from 'react';
 import s from "./Friends.module.css";
 import FriendsItem from './FriendsItem/FriendsItem';
 
-// const Friends = (props) => {
-//         let friendsElements = props.state.friends.map( 
-//                             f => < FriendsItem name = {f.name} avatar = {f.avatar} /> );
-//
-//     return(
-//         <div>
-//                 {friendsElements}
-//         </div>
-//     )
-// }
-// export default Friends;
-
 const Friends = (props) => {
-    let friendsElements = props.state.friends.map( 
-    f => { 
-        return ( f.name, <img src={f.avatar}/>) 
-               
-    } 
-        );
+        let friendsElements = props.state.friends.map( 
+                            f => < FriendsItem name = {f.name} avatar = {f.avatar} /> );
 
-return(
-    <div>
-            {friendsElements}
-    </div>
-)
+    return(
+        <div>
+                {friendsElements}
+        </div>
+    )
 }
 export default Friends;
 
 
 
-// sidebarPage: {
-//     friends: [
-//         {id: 1, name: 'Dimych', 
-//         avatar: 'https://moemisto.ua/img06'},
-
-//         {id: 2, name: 'Anton', 
-//         avatar: 'https://fs01.vseos8/0.jpg'},
-
-//         {id: 3, name: 'Sveta', 
-//         avatar: 'https://cdn.shopify.co418'},
-//          ],
-// }
