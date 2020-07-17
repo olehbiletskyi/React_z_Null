4ae8d6a90fb6b9ab0e7de6c4e7a6e4e338a819e8
OlegBiletsky <ol.biletsky@gmail.com>
2020-07-17 14:37:11 +0300

    Треба почати 43 урок!

--- src/index.js
+++ src/index.js
@@ -4,26 +4,28 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 
-
-let rerenderEntireTree = (state) => {
+//ініціалізуємо функцію "РЕРЕНДЕРИНГУ АПЛІКАЦІЇ ПОВНІСТЮ". Тобто вона малює всі наші компоненти.
+//їй ми передаємо параметр (СТАН-state)
+let rerenderEntireTree = (parameter) => {
     ReactDOM.render(
         <React.StrictMode>
             <BrowserRouter>
-                            <App 
-                                    state={state} 
-                                    // addPost={store.addPost.bind(store)} 
-                                    // updateNewPostText={store.updateNewPostText.bind(store)} 
+                            <App
+                                    state={parameter}
                                     dispatch = {store.dispatch.bind(store)}
                             />
-            </BrowserRouter>      
+            </BrowserRouter>
         </React.StrictMode>,
         document.getElementById('root')
                     );
 }
-rerenderEntireTree(store.getState());
+
+//викликаємо Ререндеринг передавши їй СТАН. СТАН ми викликаємо функцією getState() із store.
+//(Ререндеринг викликається підписниками після того, як пройшли якісь зміни)
+//store ми імпортуємо із redux-store.js. Див.там.
+rerenderEntireTree(store.getState());//store має вбудовану функцію getState(), яка повертає нам стейт
 
 store.subscribe ( () => {
-    let state = store.getState();
-    rerenderEntireTree(state);
-} );
+    rerenderEntireTree(store.getState());
+    } );
 
--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -2,18 +2,14 @@ import { createStore, combineReducers } from "redux";
 import profileReducer from "./profile-reducer";
 import dialogsReducer from "./dialogs-reducer";
 import sidebarReducer from "./sidebar-reducer";
-
+//спеціальна функція комбінує всі редюсери в один.
 let reducers = combineReducers({
     profilePage : profileReducer,
     dialogsPage : dialogsReducer,
     sidebarPage : sidebarReducer,
 });
 
-
-
-
-
+//спеціальна функція createStore, яка створює store з того reducers, який ми зкомбайнили вище.
 let store = createStore(reducers);
 
-
-export default store;
+export default store; //експортуємо store
